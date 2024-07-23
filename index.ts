360a33dcd5c6c68d0580317f2d117944a7c9307e
Robert Wilkinson <robert.wilkinson@raytheon.co.uk>
2024-07-23 11:28:47 +0100

    get it to dump anomaly and first root cause

--- index.ts
+++ index.ts
@@ -2,6 +2,10 @@ import { Context, APIGatewayProxyResult, APIGatewayEvent } from 'aws-lambda';
 
 export const handler = async (event: APIGatewayEvent, context: Context): Promise<APIGatewayProxyResult> => {
     console.log(`Event: ${JSON.stringify(event, null, 2)}`);
+    console.log(`Anomalies`);
+    console.dir(event.Anomalies[0]);
+    console.log(`First root cause`);
+    console.dir(event.Anomalies[0].RootCauses[0]);
     console.log(`Context: ${JSON.stringify(context, null, 2)}`);
     return {
         statusCode: 200,
