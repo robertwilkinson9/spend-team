0404cd84311ba7203abe910fcce11773ca34e9ac
Robert Wilkinson <robert.wilkinson@raytheon.co.uk>
2024-08-21 10:48:26 +0100

    will need aws_lambda_permission.with_sns so the lambda can be triggered by sns

--- main.tf
+++ main.tf
@@ -390,6 +390,18 @@ resource "aws_sns_topic_subscription" "team_spend_action_lambda_subscription" {
   endpoint  = "arn:aws:lambda:eu-west-2:${var.awsID}:function:team-spend-action"
 }
 
+# need to add permissions for sns to invoke the lambda too
+# https://stackoverflow.com/questions/66853200/how-to-create-aws-lambda-trigger-in-terraform
+
+resource "aws_lambda_permission" "with_sns" {
+  statement_id  = "AllowExecutionFromSNS"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.terraform_function.function_name
+# resource "aws_lambda_function" "terraform_function" {
+#  function_name    = "team-spend-action"
+  principal     = "sns.amazonaws.com"
+  source_arn    = aws_sns_topic.team_spend_action.arn
+}
 #aws sns list-subscriptions | grep Spend
 #             "SubscriptionArn": "arn:aws:sns:eu-west-2:778666285893:TeamSpendAction:45909378-bd41-4d4d-afb1-96a17bd9eecc",
 #             "TopicArn": "arn:aws:sns:eu-west-2:778666285893:TeamSpendAction"
