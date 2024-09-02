467d27ce16a4b2a4bdc89218f109903ca6f0b06c
Robert Wilkinson <robert.wilkinson@raytheon.co.uk>
2024-09-02 11:37:04 +0100

    drop comments since they are now redundant

--- modules/anomaly_monitor_response/variables.tf
+++ modules/anomaly_monitor_response/variables.tf
@@ -1,17 +1,11 @@
 variable "awsID" {
   description = "the AWS ID to use for all resources"
   type = number
-# it is a number but should it be a string? XXX
   default = 0
 }
-
-# I guess that these email addresses should have the capacity
-# to be lists - or something? XXX
 
 variable "team_spend_emails" {
   description = "the email address for alerts"
   type = list(string)
   default = [""]
-#  type = string
-#  default = ""
 }
